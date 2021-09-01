0767597940a2f52a3613e0566d0ac8bdb4980f8f
Shuvomoy Sharma <63641315+Shuvomoy-Sharma@users.noreply.github.com>
2021-09-01 21:50:44 +0530

    Update 1 class.py

--- 1 class.py
+++ 1 class.py
@@ -2,13 +2,14 @@
  # what is init method or constructor
  #self
 class Person:
-	def __init__(self,firstname,lastname,age):
+	def __init__(self,firstname,lastname,age,dob):
 		# instance variabel
 		self.firstname=firstname
 		self.lastname=lastname
 		self.age=age
+		self.dob=dob
 # creating object
-p1=Person('Victor','sen',22)
+p1=Person('Victor','sen',22,'1900-02-02')
 # p2=Person('Maria','khatun',20)
 print(p1.firstname,p1.lastname,p1.age)
 # print(p2.firstname,p2.lastname,p2.age)					
